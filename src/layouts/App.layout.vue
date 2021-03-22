c91495882d678b53e29baa13a77454c7609a2049
Jose Jurado <jjuradogi@gmai.com>
2021-03-22 14:35:46 +0100

    Add App Layuot

---
+++ src/layouts/App.layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-layout-container">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AppLayout extends Vue {}
+</script>
+
+<style lang="scss">
+.app-layout-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
