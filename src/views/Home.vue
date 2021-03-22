c42ca5169fdcf4aaa5f99faed689f81791c91961
Jose Jurado <jjuradogi@gmai.com>
2021-03-22 14:37:20 +0100

    Add components to main view with mock getData

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="home-container container-fluid justify-content-center align-items-center">
+    <div>
+      <add-todo />
+      <todo-list :todo-list="todoList" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { Action, State } from 'vuex-class';
 
-@Component
-export default class Home extends Vue {}
+import { ITodoItem } from '@/models';
+
+import AddToDo from '@/components/ToDo/AddToDo/AddToDo.component.vue';
+import ToDoList from '@/components/ToDo/ToDoList/ToDoList.component.vue';
+
+@Component({
+  components: {
+    'todo-list': ToDoList,
+    'add-todo': AddToDo
+  }
+})
+export default class Home extends Vue {
+  @State(state => state.todo.todoList) todoList!: ITodoItem[];
+
+  @Action('todo/getToDoList')
+  private getToDoList!: () => Promise<void>;
+
+  async getData(): Promise<void> {
+    await this.getToDoList();
+  }
+
+  async mounted(): Promise<void> {
+    await this.getData();
+  }
+}
 </script>
+
+<style lang="scss">
+.home-container {
+  height: 100%;
+  display: flex;
+}
+</style>
